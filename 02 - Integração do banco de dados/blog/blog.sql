60b103664593dfb60802891ef21b7cec54655167
Nícolas Guedes <nicolastrocatti@gmail.com>
2025-03-27 20:12:59 -0300

    Configuração das rotas do projeto do blog

---
+++ 02 - Integração do banco de dados/blog/blog.sql
@@ -0,0 +1,92 @@
+CREATE DATABASE blog;
+USE blog;
+
+CREATE TABLE `postagens` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `titulo` VARCHAR(200) NOT NULL,
+  `conteudo` longtext,
+  `autor_id` INT NOT NULL,
+  `imagem_capa` TEXT,
+  `postado_em` datetime NOT NULL,
+  `categoria_id` INT NOT NULL,
+  `regiao_id` INT NOT NULL,
+  `ativo` BOOLEAN DEFAULT TRUE	
+);
+
+CREATE TABLE `usuarios` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `usuario` VARCHAR(20) UNIQUE NOT NULL,
+  `senha` VARCHAR(255) NOT NULL,
+  `nome` VARCHAR(100) NOT NULL,
+  `data_criacao` DATETIME NOT NULL,
+  `data_nascimento` DATE NOT NULL
+);
+
+CREATE TABLE `autores` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `usuario_id` INT UNIQUE NOT NULL,
+  `biografia` longtext,
+  `imagem` text,
+  `links` text
+);
+
+CREATE TABLE `categorias` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `categoria` VARCHAR(100) NOT NULL,
+  `slug` VARCHAR(100)
+);
+
+CREATE TABLE `regioes` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `regiao` VARCHAR(100) NOT NULL,
+  `slug` VARCHAR(100)
+);
+
+CREATE TABLE `comentarios` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `comentario` VARCHAR(255) NOT NULL,
+  `usuario_id` INT NOT NULL,
+  `postado_em` DATETIME NOT NULL,
+  `postagem_id` INT NOT NULL,
+  `ativo` BOOLEAN DEFAULT TRUE
+);
+
+ALTER TABLE `postagens` ADD FOREIGN KEY (`autor_id`) REFERENCES `autores` (`id`);
+
+ALTER TABLE `postagens` ADD FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`);
+
+ALTER TABLE `postagens` ADD FOREIGN KEY (`regiao_id`) REFERENCES `regioes` (`id`);
+
+ALTER TABLE `autores` ADD FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+
+ALTER TABLE `comentarios` ADD FOREIGN KEY (`postagem_id`) REFERENCES `postagens` (`id`);
+
+ALTER TABLE `comentarios` ADD FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+
+INSERT INTO categorias(categoria,slug) VALUES
+('Turismo e Natureza', 'turismo-e-natureza'),
+('Praia', 'praia'),
+('Radical', 'radical'),
+('Gastronômico', 'gastronomico');
+
+INSERT INTO regioes(regiao,slug) VALUES
+('Caminhos do Alto Vale', 'caminhos-do-alto-vale'),
+('Vale do Contestado', 'vale-do-contestado'),
+('Vale Europeu', 'vale-europeu'),
+('Grande Oeste', 'grande-oeste'),
+('Caminho dos Príncipes', 'caminho-dos-principes'),
+('Caminhos da Fronteira', 'caminhos-da-fronteira'),
+('Encantos do Sul', 'encantos-do-sul'),
+('Costa Verde e Mar', 'costa-verde-e-mar'),
+('Caminhos dos Canyons', 'caminhos-dos-canyons'),
+('Serra Catarinense', 'serra-catarinense'),
+('Grande Florianópolis', 'grande-florianopolis'),
+('Vale das Águas', 'vale-das-aguas');
+
+select * from categorias;
+select * from regioes;
+
+select * from autores;
+
+
+
--- 02 - Integração do banco de dados/blog/server.js
+++ 02 - Integração do banco de dados/blog/server.js
@@ -2,6 +2,8 @@ import mysql from 'mysql2'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import express, { query } from 'express'
+import slugify from 'slugify'
+import bcrypt from 'bcryptjs'
 
 // Carregar dados do .env
 dotenv.config();
@@ -10,7 +12,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-const port = 3303;
+const port = 3301;
 
 // Criar conexão
 const connection = mysql.createConnection({
@@ -30,27 +32,271 @@ connection.connect((error) => {
     }
 })
 
+
+// * Rotas de seleção dos contéudos das tabelas
 app.get('/categorias', (req, res) => {
-    let query = 'SELECT categoria FROM categorias';
+    const query = 'SELECT categoria FROM categorias';
     connection.query(query, (error, result) => {
         if (error) {
             return res.status(404).json({ error: 'Não encontrado' })
         }
         return res.status(200).json({ categorias: result })
     })
-})
+});
 
 app.get('/regioes', (req, res) => {
-    let query = 'SELECT regiao FROM regioes';
+    const query = 'SELECT regiao FROM regioes';
     connection.query(query, (error, result) => {
         if (error) {
             return res.status(404).json({ error: 'Não encontrado' })
         }
-        return res.status(200).json({ categorias: result })
+        return res.status(200).json({ regiao: result })
     })
-})
+});
+
+// * Rotas de Criação de contéudos das tabelas
+app.post('/cadastrar/categorias', (req, res) => {
+    const { categoria } = req.body;
+    const slug = slugify(categoria, { lower: true, strict: true });
+
+    const query = 'INSERT INTO categorias (categoria,slug) VALUES (?,?)';
+    connection.query(query, [categoria, slug], (error, result) => {
+        if (error) {
+            return res.status(500).json({ error: error })
+        }
+        return res.status(201).json({ message: 'Inserido com sucesso', id: result.insertId })
+    })
+});
+
+app.post('/cadastrar/regioes', (req, res) => {
+    const { regiao } = req.body;
+    const slug = slugify(regiao, { lower: true, strict: true });
+
+    const query = 'INSERT INTO regioes (regiao,slug) VALUES (?,?)';
+    connection.query(query, [regiao, slug], (error, result) => {
+        if (error) {
+            return res.status(500).json({ error: error })
+        }
+        return res.status(201).json({ message: 'Inserido com sucesso', id: result.insertId })
+    })
+});
+
+app.post('/cadastrar/usuario', async (req, res) => {
+    const { usuario, senha, nome, data_nascimento } = req.body;
+    try {
+        const salt = await bcrypt.genSalt()
+        const senhaCriptografada = await bcrypt.hash(senha, salt)
+
+        const query = 'INSERT INTO usuarios (usuario, senha , nome , data_criacao, data_nascimento) VALUES (?,?,?,NOW(),?)'
+        connection.query(query, [usuario, senhaCriptografada, nome, data_nascimento], (error, result) => {
+            if (error) {
+                return res.status(500).json({ error: error, message: 'Erro ao inserir o usuario' })
+            }
+            return res.status(201).json({ message: 'Cadastrado com sucesso', id: result.insertId })
+        })
+    }
+    catch (error) {
+        res.status(500).json({ error: error, message: 'Erro ao processar a senha' })
+    }
+});
+
+app.post('/cadastrar/autor', (req, res) => {
+    const { usuario_id, biografia, imagem, links } = req.body;
+
+    connection.query("select id from usuarios where id = ?", [usuario_id], (error, result) => {
+        if (error) return res.status(500).json({ error: error, message: 'Erro ao procurar o usuario' })
+        if (result.length === 0) return res.status(401).json({ message: 'Usuário não encontrado', id: result.insertId })
+
+        const query = "INSERT INTO autores(usuario_id,biografia,imagem,links) VALUES (?,?,?,?)";
+        connection.query(query, [usuario_id, biografia, imagem, links], (error, result) => {
+            if (error) {
+                return res.status(500).json({ error: error, message: 'Erro ao inserir o autor' })
+            }
+            return res.status(201).json({ message: 'Cadastrado com sucesso', id: result.insertId })
+        })
+    })
+});
+
+app.post('/criar/postagem', (req, res) => {
+    const { titulo, conteudo, autor_id, imagem_capa, categoria_id, regiao_id, ativo } = req.body;
+
+    connection.query("select id from autores where id = ?", [autor_id], (error, result) => {
+        if (error) return res.status(500).json({ error: error, message: 'Erro ao procurar o usuario' })
+        if (result.length === 0) return res.status(401).json({ message: 'Usuário não encontrado', id: result.id })
+
+        const query = "INSERT INTO postagens(titulo,conteudo,autor_id,imagem_capa,postado_em,categoria_id,regiao_id,ativo) VALUES (?,?,?,?,curdate(),?,?,?)";
+        connection.query(query, [titulo, conteudo, autor_id, imagem_capa, categoria_id, regiao_id, ativo], (error, result) => {
+            if (error) {
+                return res.status(500).json({ error: error, message: 'Erro ao criar a postagem' })
+            }
+            return res.status(201).json({ message: 'Postagem criada com sucesso', id: result.insertId })
+        })
+    })
+});
+
+app.post('/criar/comentario', (req, res) => {
+    const { comentario, usuario_id, postagem_id, ativo } = req.body;
+
+    connection.query("select id from usuarios where id = ?", [usuario_id], (error, result) => {
+        if (error) return res.status(500).json({ error: error, message: 'Erro ao procurar o usuario' })
+        if (result.length === 0) return res.status(401).json({ message: 'Usuário não encontrado', id: result.id })
+
+        const query = "INSERT INTO comentarios(comentario,usuario_id,postado_em,postagem_id,ativo) VALUES (?,?,curdate(),?,?)";
+        connection.query(query, [comentario, usuario_id, postagem_id, ativo], (error, result) => {
+            if (error) {
+                return res.status(500).json({ error: error, message: 'Erro ao criar o comentário' })
+            }
+            return res.status(201).json({ message: 'comentário criado com sucesso', id: result.insertId })
+        })
+    })
+});
+
+// * Rotas de atualização do conteúdo das tabelas
+app.put("/atualizar/categoria/:id", (req, res) => {
+    const  id  = req.params.id;
+    const { categoria } = req.body;
+    const slug = slugify(categoria, { lower: true, strict: true });
+
+    const query = 'update categorias set categoria = ?, slug = ? where id = ?';
+    connection.query(query, [categoria, slug, id], (error, result) => {
+        if (error) {
+            return res.status(500).json({ error: error })
+        }
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: 'Categoria não encontrada' })
+        };
+
+        return res.status(200).json({ message: 'atualizado com sucesso', id: id })
+    })
+});
+
+app.put("/atualizar/regioes/:id", (req, res) => {
+    const  id  = req.params.id;
+    const { regiao } = req.body;
+    const slug = slugify(regiao, { lower: true, strict: true });
+
+    const query = 'update regioes set regiao = ?, slug = ? where id = ?';
+    connection.query(query, [regiao, slug, id], (error, result) => {
+        if (error) {
+            return res.status(500).json({ error: error })
+        }
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: 'Região não encontrada' })
+        };
+
+        return res.status(200).json({ message: 'atualizado com sucesso', id: id })
+    })
+});
+
+app.put("/atualizar/usuarios/:id", async (req, res) => {
+    try {
+        const  id  = req.params.id;
+        const {  usuario, senha, nome, data_nascimento } = req.body;
+
+        const salt = await bcrypt.genSalt()
+        const senhaCriptografada = await bcrypt.hash(senha, salt)
+
+        const query = 'update usuarios set usuario = ?, senha = ?, nome = ?, data_nascimento = ? where id = ?'
+        connection.query(query, [usuario, senhaCriptografada, nome, data_nascimento, id ], (error, result) => {
+            if (error) {
+                return res.status(500).json({ error: error, message: 'Erro ao inserir o usuario' })
+            }
+            return res.status(201).json({ message: 'Usuário atualizado com sucesso', id: id })
+        })
+    }
+    catch(error) {
+        res.status(500).json({ error: error, message: 'Erro ao processar a senha' })
+    }
+});
+
+app.put("/atualizar/autores/:id", (req, res) => {
+    const  id  = req.params.id;
+    const { usuario_id, biografia, imagem, links } = req.body;
+
+    const query = 'update autores set usuario_id = ?, biografia = ?, imagem = ?, links = ? where id = ?';
+    connection.query(query, [usuario_id, biografia, imagem, links, id], (error, result) => {
+        if (error) {
+            return res.status(500).json({ error: error })
+        }
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: 'Autor não encontrada' })
+        };
+
+        return res.status(200).json({ message: 'Autor atualizado com sucesso', id: id })
+    })
+});
+
+app.put("/atualizar/postagem/:id", (req, res) => {
+    const  id  = req.params.id;
+    const { titulo, conteudo, autor_id, imagem_capa, categoria_id, regiao_id  } = req.body;
+
+    const query = 'update postagens set titulo = ?, conteudo = ?, autor_id = ?, imagem_capa = ?, categoria_id = ?, regiao_id = ? where id = ?';
+    connection.query(query, [titulo, conteudo, autor_id, imagem_capa, categoria_id, regiao_id , id], (error, result) => {
+        if (error) {
+            return res.status(500).json({ error: error })
+        }
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: 'Postagem não encontrada' })
+        };
+
+        return res.status(200).json({ message: 'Postagem atualizado com sucesso', id: id })
+    })
+});
+
+app.put("/atualizar/comentario/:id", (req, res) => {
+    const  id  = req.params.id;
+    const { comentario, usuario_id, postagem_id  } = req.body;
+
+    const query = 'update comentarios set comentario = ?, usuario_id = ?, postagem_id = ? where id = ?';
+    connection.query(query, [comentario, usuario_id, postagem_id, id], (error, result) => {
+        if (error) {
+            return res.status(500).json({ error: error })
+        }
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: 'Comentário não encontrada' })
+        };
+
+        return res.status(200).json({ message: 'Comentário atualizado com sucesso', id: id })
+    })
+});
+
+// * Seção de alterar os Status dos comentários e das publicações
+app.put("/status/postagem/:id", (req, res) => {
+    const  id  = req.params.id;
+    const { ativo } = req.body;
+
+    const query = 'update postagens set ativo = ? where id = ?';
+    connection.query(query, [ativo, id], (error, result) => {
+        if (error) {
+            return res.status(500).json({ error: error })
+        }
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: 'Comentário não encontrada' })
+        };
+
+        return res.status(200).json({ message: 'Publicação atualizado com sucesso', id: id })
+    })
+});
+
+app.put("/status/comentario/:id", (req, res) => {
+    const  id  = req.params.id;
+    const { ativo } = req.body;
+
+    const query = 'update comentarios set ativo = ? where id = ?';
+    connection.query(query, [ativo, id], (error, result) => {
+        if (error) {
+            return res.status(500).json({ error: error })
+        }
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: 'Comentário não encontrada' })
+        };
+
+        return res.status(200).json({ message: 'Comentário atualizado com sucesso', id: id })
+    })
+});
+
 
 app.listen(port, () => {
     console.log('Servidor Rodando.');
     console.log(`http://localhost:${port}`)
-})
+});
