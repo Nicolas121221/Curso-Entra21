4dddbdbc0ae40ef29a8397b0fd14b888567024a6
Nícolas Guedes <nicolastrocatti@gmail.com>
2025-05-24 20:51:51 -0300

    feat: adds the main page into the blog exercise

--- 04 - Frameworks/01 - svelte/frontend/src/routes/+page.svelte
+++ 04 - Frameworks/01 - svelte/frontend/src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main
+	class="fixed bottom-0 left-0 right-0 top-0 -z-10 flex items-center justify-center bg-zinc-800 pt-12"
+>
+	<section
+		class="w-4xl flex h-screen flex-col items-center justify-center gap-5 font-sans text-white"
+	>
+		<h1 class="w-xl text-5xl">Atividade Criando um Blog Com svelte</h1>
+		<p class="w-xl indent-10 text-lg">
+			Essa atividade foi feita utilizando o svelte e tailwind css para fazer um fetch com Json
+			Placeholder para criar a lógica de como seria um fetch com um banco de dados, para exemplo
+			utilizarei o onMount do svelte para carregar os dados e em seguida mostrar os cards.
+		</p>
+		<br />
+		<p>
+			Clique aqui para ver os <a href="/posts" class="text-blue-700 duration-150 hover:underline"
+				>Posts</a
+			>.
+		</p>
+	</section>
+</main>
---
+++ 04 - Frameworks/01 - svelte/frontend/src/routes/posts/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { onMount } from "svelte";
+
+	let loading = $state(true);
+    let data = []
+    onMount(()=>{
+        
+
+
+
+    })
+</script>
+
+<main
+	class="fixed bottom-0 left-0 right-0 top-0 -z-10 flex items-center justify-center bg-zinc-800 pt-12"
+>
+	{#if loading}
+		<section class="flex flex-col items-center justify-center gap-4">
+			<div
+				class="flex size-10 animate-spin items-center justify-center rounded-full border border-l-0 border-r-0 border-t-0 border-b-blue-500"
+			></div>
+			<h3 class="text-2xl font-thin text-white">Carregando ...</h3>
+		</section>
+	{:else}
+    <div>
+
+    </div>
+    {/if}
+</main>
