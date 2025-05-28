6c37aa7f5a318cfd21a3a460c997b175eda30edb
Nícolas Guedes <nicolastrocatti@gmail.com>
2025-05-27 23:42:04 -0300

    feat: adds a component for the comments

---
+++ 04 - Frameworks/01 - svelte/frontend/src/lib/components/Comment.svelte
@@ -0,0 +1,8 @@
+<script>
+	let { id, name, email, body } = $props();
+</script>
+
+<section class="mb-3 rounded-sm bg-zinc-900/50 p-6 font-sans text-lg text-white">
+	<h5 class="w-full font-bold"><span class="text-4xl text-blue-700 mr-2">{id}</span>{name}<span class="text-xs opacity-25 ml-2">{email}</span></h5>
+	<p class="w-2xl mt-2">{body}</p>
+</section>
