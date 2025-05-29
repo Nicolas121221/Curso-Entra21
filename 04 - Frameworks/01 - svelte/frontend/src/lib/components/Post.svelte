ed01e69d3b152d54ba3abfaf08acb9925610c761
Nícolas Guedes <nicolastrocatti@gmail.com>
2025-05-28 23:30:24 -0300

    feat: adds the working comments to the svelte exercise

--- 04 - Frameworks/01 - svelte/frontend/src/lib/components/Post.svelte
+++ 04 - Frameworks/01 - svelte/frontend/src/lib/components/Post.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <section class="w-3xl font-sans text-lg my-10">
-	<h4 class="text-white w-2xl text-xl capitalize hover:text-blue-600 duration-150 mb-2"><a href="/comments/{id}">{title}</a></h4>
+	<h4 class="text-white w-2xl text-xl capitalize hover:text-blue-600 duration-150 mb-2"><a href="/posts/{id}">{title}</a></h4>
     <p class="indent-10 w-sm text-gray-400 text-justify">{body}</p>
 </section>
